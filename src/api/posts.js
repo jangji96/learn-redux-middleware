0e33691c484269ab8cdf7d927dcbfa950a90a8dd
jangji96 <xxwlgns@naver.com>
2021-06-29 16:53:34 +0900

    redux-thunk로 프로미스 다루기 - 2

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,43 +1,34 @@
-import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
-import {
-  createPromiseThunk,
-  reducerUtils,
-  handleAsyncActions
-} from '../lib/asyncUtils';
+// n 밀리세컨드동안 기다리는 프로미스를 만들어주는 함수
+const sleep = n => new Promise(resolve => setTimeout(resolve, n));
 
-/* 액션 타입 */
+// 가짜 포스트 목록 데이터
+const posts = [
+  {
+    id: 1,
+    title: '리덕스 미들웨어를 배워봅시다',
+    body: '리덕스 미들웨어를 직접 만들어보면 이해하기 쉽죠.'
+  },
+  {
+    id: 2,
+    title: 'redux-thunk를 사용해봅시다',
+    body: 'redux-thunk를 사용해서 비동기 작업을 처리해봅시다!'
+  },
+  {
+    id: 3,
+    title: 'redux-saga도 사용해봅시다',
+    body:
+      '나중엔 redux-saga를 사용해서 비동기 작업을 처리하는 방법도 배워볼 거예요.'
+  }
+];
 
-// 포스트 여러개 조회하기
-const GET_POSTS = 'GET_POSTS'; // 요청 시작
-const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'; // 요청 성공
-const GET_POSTS_ERROR = 'GET_POSTS_ERROR'; // 요청 실패
-
-// 포스트 하나 조회하기
-const GET_POST = 'GET_POST';
-const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
-const GET_POST_ERROR = 'GET_POST_ERROR';
-
-// 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
-export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
-
-// initialState 쪽도 반복되는 코드를 initial() 함수를 사용해서 리팩토링 했습니다.
-const initialState = {
-  posts: reducerUtils.initial(),
-  post: reducerUtils.initial()
+// 포스트 목록을 가져오는 비동기 함수
+export const getPosts = async () => {
+  await sleep(500); // 0.5초 쉬고
+  return posts; // posts 배열
 };
 
-export default function posts(state = initialState, action) {
-  switch (action.type) {
-    case GET_POSTS:
-    case GET_POSTS_SUCCESS:
-    case GET_POSTS_ERROR:
-      return handleAsyncActions(GET_POSTS, 'posts')(state, action);
-    case GET_POST:
-    case GET_POST_SUCCESS:
-    case GET_POST_ERROR:
-      return handleAsyncActions(GET_POST, 'post')(state, action);
-    default:
-      return state;
-  }
-}
+// ID로 포스트를 조회하는 비동기 함수
+export const getPostById = async id => {
+  await sleep(500); // 0.5초 쉬고
+  return posts.find(post => post.id === id); // id 로 찾아서 반환
+};
