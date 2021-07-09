019794d3ec61f4ccfbded41f7401f67834cf936f
jangji96 <xxwlgns@naver.com>
2021-07-09 11:32:22 +0900

    saga에서 라우터 연동하기

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -8,7 +8,7 @@ import {
   createPromiseSaga,
   createPromiseSagaById
 } from '../lib/asyncUtils';
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, getContext } from 'redux-saga/effects';
 
 /* 액션 타입 */
 
@@ -21,6 +21,7 @@ const GET_POSTS_ERROR = 'GET_POSTS_ERROR'; // 요청 실패
 const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
+const GO_TO_HOME = 'GO_TO_HOME';
 
 // 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
 // export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
@@ -29,6 +30,7 @@ const GET_POST_ERROR = 'GET_POST_ERROR';
 export const getPosts = () => ({ type: GET_POSTS });
 // payload는 파라미터 용도, meta는 리듀서에서 id를 알기위한 용도
 export const getPost = id => ({ type: GET_POST, payload: id, meta: id });
+export const goToHome = () => ({ type: GO_TO_HOME });
 
 // function* getPostsSaga() {
 //   try {
@@ -69,17 +71,22 @@ export const getPost = id => ({ type: GET_POST, payload: id, meta: id });
 
 const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
 const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
+function* goToHomeSaga() {
+  const history = yield getContext('history');
+  history.push('/');
+}
 
 // 사가들을 합치기
 export function* postsSaga() {
   yield takeEvery(GET_POSTS, getPostsSaga);
   yield takeEvery(GET_POST, getPostSaga);
+  yield takeEvery(GO_TO_HOME, goToHomeSaga);
 }
 
 // 3번째 인자를 사용하면 withExtraArgument 에서 넣어준 값들을 사용 할 수 있습니다.
-export const goToHome = () => (dispatch, getState, { history }) => {
-  history.push('/');
-};
+// export const goToHome = () => (dispatch, getState, { history }) => {
+//   history.push('/');
+// };
 
 // initialState 쪽도 반복되는 코드를 initial() 함수를 사용해서 리팩토링 했습니다.
 const initialState = {
